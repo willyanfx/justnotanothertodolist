ccd5ff55159aeb5c0d79a87766c1d982c4789c26
Willyan Geraldo <willyangeraldo@Willyans-MacBook-Pro.local>
2020-02-09 22:00:00 -0800

    drag & drop

--- src/components/tasks.tsx
+++ src/components/tasks.tsx
@@ -1,17 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Checkbox } from './Checkbox';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const initial = Array.from({ length: 10 }, (v, k) => k).map(k => {
+    const custom = {
+        id: `id-${k}`,
+        content: `Quote ${k}`
+    };
+
+    return custom;
+});
+
+const grid = 8;
+
+function ListItem({ item, index }: any) {
+    return (
+        <Draggable draggableId={item.id} index={index}>
+            {provided => (
+                <TodoItems
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}>
+                    <Checkbox />
+                    <span>{item.content}</span>
+                </TodoItems>
+            )}
+        </Draggable>
+    );
+}
+
+// Ensuring the whole list does not re-render when the droppable re-renders
+const TasksList = React.memo(function TasksList({ items }: any) {
+    return items.map((item: { id: number }, index: number) => (
+        <ListItem item={item} index={index} key={item.id} />
+    ));
+});
+
+const reorder = (
+    list: Iterable<unknown> | ArrayLike<unknown>,
+    startIndex: any,
+    endIndex: any
+) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
 
 export const Tasks = () => {
+    const [tasks, setTasks] = useState(initial);
+
+    const onDragEnd = (result: any) => {
+        if (!result.destination) {
+            return;
+        }
+
+        if (result.destination.index === result.source.index) {
+            return;
+        }
+
+        const newList: any = reorder(
+            tasks,
+            result.source.index,
+            result.destination.index
+        );
+
+        setTasks(newList);
+    };
+
     return (
         <TaskDisplay>
             <h2>Project name</h2>
-            <TaskList>
-                <li>
-                    <Checkbox />
-                    <span>task</span>
-                </li>
-            </TaskList>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable droppableId='list'>
+                    {provided => (
+                        <TaskList
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}>
+                            <TasksList items={tasks} />
+                            {provided.placeholder}
+                        </TaskList>
+                    )}
+                </Droppable>
+            </DragDropContext>
         </TaskDisplay>
     );
 };
@@ -21,7 +94,6 @@ const TaskDisplay = styled.div`
     background-color: white;
     margin-left: 266px;
     border-right: 3px;
-    min-height: calc(100vh);
     vertical-align: top;
     padding-left: 40px;
     padding-right: 40px;
@@ -34,8 +106,10 @@ const TaskDisplay = styled.div`
         margin: 0 30px 20px 0;
     }
 `;
-const TaskList = styled.ul`
+
+const TaskList = styled.div`
     display: flex;
+    flex-direction: column;
     line-height: 18px;
     color: #333;
     padding-top: 10px;
@@ -43,4 +117,13 @@ const TaskList = styled.ul`
     font-size: 14px;
     list-style-type: none;
     border-bottom: 1px solid #f0f0f0;
+`;
+
+const TodoItems = styled.div`
+    display: flex;
+    width: 100%;
+    border: 1px solid grey;
+    margin-bottom: ${grid}px;
+    background-color: #fff;
+    padding: ${grid}px;
 `;
