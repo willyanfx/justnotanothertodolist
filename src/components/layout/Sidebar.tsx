a4bf6d211b5503c1b9a49aba615061dd6e439653
Willyanfx <willyan.fx@gmail.com>
2020-02-11 23:12:14 -0800

    sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -5,33 +5,29 @@ import { css, jsx } from '@emotion/core';
 export const Sidebar = () => {
     return (
         <div css={styleSidebar}>
-            <ul>
-                <li>
-                    <div>
-                        <i>📥</i>
-                        <span>Inbox</span>
-                    </div>
+            <ul css={styleUl}>
+                <li css={styleIl}>
+                    <i css={styleIcon}>📥</i>
+                    <span css={styleContent}>Inbox</span>
                 </li>
-                <li>
-                    <div>
-                        <i>📆</i>
-                        <span>Today</span>
-                    </div>
+                <li css={styleIl}>
+                    <i css={styleIcon}>📆</i>
+                    <span css={styleContent}>Today</span>
                 </li>
-                <li>
-                    <div>
-                        <i>🗓</i>
-                        <span>Next 7 days</span>
-                    </div>
+                <li css={styleIl}>
+                    <i css={styleIcon}>🗓</i>
+                    <span css={styleContent}>Next 7 days</span>
                 </li>
-                <div>
-                    <span>
-                        <h2>Projects</h2>
-                    </span>
-                    <span>▾</span>
+                <header css={styleProjects}>
+                    <button>
+                        <span>
+                            <h2>Projects</h2>
+                        </span>
+                        <span>▾</span>
+                    </button>
 
                     <ul></ul>
-                </div>
+                </header>
             </ul>
         </div>
     );
@@ -40,12 +36,66 @@ export const Sidebar = () => {
 const styleSidebar = css`
     width: 266px;
     height: calc(100vh);
-    padding-top: 74px;
+    padding-top: 62px;
     position: fixed;
     overflow-x: hidden;
     overflow-y: hidden;
     border-right: 3px;
     background-color: #fafafa;
 `;
-const styleUl = css``;
-const styleProjects = css``;
+const styleUl = css`
+    margin: 0;
+    padding: 0;
+`;
+const styleIl = css`
+    min-height: 24px;
+    font-size: 14px;
+    list-style: none;
+    cursor: pointer;
+    padding: 5px 16px 5px 5px;
+    transition: color 0.1s ease-in, background-color 0.1s ease-in;
+    display: flex;
+    border-radius: 3px;
+    align-items: center;
+`;
+
+const styleIcon = css`
+    width: 28px;
+    height: 24px;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 5px;
+    margin-top: -1px;
+    font-size: 14px;
+    color: #555;
+`;
+const styleContent = css`
+    display: inline-block;
+    width: 187px;
+    vertical-align: top;
+    min-height: 24px;
+    line-height: 24px;
+    word-break: break-all;
+    word-break: break-word;
+`;
+
+const styleProjects = css`
+    padding: 1px 0;
+    position: relative;
+    display: flex;
+    font-size: 14px;
+    line-height: 1em;
+    cursor: pointer;
+    button {
+        flex: 1;
+        display: flex;
+        text-align: left;
+        font-size: 14px;
+        color: #333;
+        font-weight: bold;
+        padding: 10px 0;
+    }
+`;
