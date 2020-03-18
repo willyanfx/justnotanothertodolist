39c96831090f5c1631acc052b27698e425928020
Willyanfx <willyan.fx@gmail.com>
2020-03-17 19:05:51 -0700

    db

--- src/db.ts
+++ src/db.ts
@@ -3,18 +3,16 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_URL,
+    projectId: process.env.REACT_APP_PROJ_ID,
+    storageBucket: process.env.REACT_APP_STORAGE,
+    messagingSenderId: process.env.REACT_APP_SENDER,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(config);
 
 export const db = firebase.firestore();
-
-export const auth = () => firebase.auth();
-
-export const mode = 'real';
+export const auth = firebase.auth();
